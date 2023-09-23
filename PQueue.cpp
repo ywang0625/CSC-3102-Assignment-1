d562c3b6f75d8655de54865e146f94322cf336d5
ywang0625 <yuehming.wang0625@gmail.com>
2023-09-22 23:56:30 -0500

    Test 1

---
+++ PQueue.cpp
@@ -0,0 +1,124 @@
+/**
+ * implementation file for the  stl vector-based heap max-priority queue class
+ * @author William Duncan, Yueh-Ming
+ * <pre>
+ * file PQueue.cpp
+ * Date: 9999-99-99
+ * Instructor: Dr. Duncan
+ * Course: csc 3102
+ * </pre>
+*/
+
+#include "PQueue.h"
+
+using namespace std;
+
+template<typename E>
+void PQueue<E>::swap(int place, int parent)     //unsure
+{
+    E* tmp = tree[place];
+    tree[place] = tree[parent];
+    tree[parent] = tmp;
+    delete tmp;
+
+}
+
+
+
+template<typename E>
+void PQueue<E>::rebuild(int root)
+{
+    //implement this function
+
+}
+
+
+
+
+template <typename E>
+PQueue<E>::PQueue()             //unsure
+{
+    //implement this function
+    tree.clear();
+}
+
+
+
+
+template <typename E>
+PQueue<E>::PQueue(std::function<bool(E,E)> fn)
+{
+    //implement this function
+
+
+}
+
+
+
+
+template <typename E>
+bool PQueue<E>::isEmpty() const             //unsure
+{
+   // implement this function
+   if (!tree)
+       return true;
+   else
+       return false;
+}
+
+
+
+
+template<typename E>
+void PQueue<E>::insert(E item)
+{
+  //implement this function
+  item = ; // implement
+}
+
+
+
+
+template<typename E>
+E PQueue<E>::remove()
+{
+   // implement this function
+
+   if (!tree)                   //unsure
+       throw PQueueException("Error from remove(): This priority queue is empty");
+
+
+   return NULL; //?? is it right
+}
+
+
+
+
+template<typename E>
+const E& PQueue<E>::peek() const
+{
+   //implement this function
+    if (!tree)                   //unsure
+        throw PQueueException("Error from peek(): This priority queue is empty");
+   return NULL;
+}
+
+
+
+
+template<typename E>
+int PQueue<E>::size()const
+{
+   //Implement this function
+    int count = 0;
+    int* pointer;
+    if ()
+
+
+   return ;
+}
+
+
+
+
+
